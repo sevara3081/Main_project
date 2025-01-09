7d31d43b2a0eb5f776245e41c35ce7459da12f25
Sevara3081 <sevara3081@mail.ru>
2025-01-09 14:57:01 +0300

    SearchSuggestionsAPI.py

---
+++ pages/SearchSuggestionsAPI.py
@@ -0,0 +1,43 @@
+import requests
+import allure
+
+class SearchSuggestionsAPI:
+    """
+    Класс для работы с API получения поисковых подсказок в интернет-магазине Читай-город.
+    """
+
+    def __init__(self, base_url, token):
+        """
+        Инициализация API клиента.
+
+        :param base_url: Базовый URL API.
+        :param token: Токен для авторизации.
+        """
+        self.base_url = base_url
+        self.token = token
+
+    @allure.step("Отправить запрос на получение поисковых подсказок")
+    def get_suggestions(self, phrase, page=1, per_page=5):
+        """
+        Получить список поисковых подсказок.
+
+        :param phrase: Фраза для поиска.
+        :param page: Номер страницы (по умолчанию 1).
+        :param per_page: Количество элементов на странице (по умолчанию 5).
+        :return: Кортеж из кода статуса ответа и тела ответа в формате JSON.
+        """
+        url = f"{self.base_url}/search/search-phrase-suggests"
+        headers = {"Authorization": f"Bearer {self.token}"}
+        params = {
+            "suggests[page]": page,
+            "suggests[per-page]": per_page,
+            "phrase": phrase,
+            "include": "products,authors,bookCycles,publisherSeries,publishers,categories",
+        }
+
+        with allure.step("Выполнить GET запрос"):
+            response = requests.get(url, headers=headers, params=params)
+            allure.attach(str(params), name="Request Parameters", attachment_type=allure.attachment_type.JSON)
+            allure.attach(response.text, name="Response Body", attachment_type=allure.attachment_type.JSON)
+
+        return response.status_code, response.json()

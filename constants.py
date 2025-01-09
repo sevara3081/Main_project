d2e48dccf9bc82e78563ea90addb44d22b03d406
Sevara3081 <sevara3081@mail.ru>
2025-01-09 14:47:22 +0300

    constants.py

---
+++ constants.py
@@ -0,0 +1,4 @@
+BASE_API_URL = "https://web-gate.chitai-gorod.ru/api/v2"
+API_URL_SEARCH = f"{BASE_API_URL}/search/search-phrase-suggests"
+BEARER_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzY0ODcxOTgsImlhdCI6MTczNjMxOTE5OCwiaXNzIjoiL2FwaS92MS9hdXRoL2Fub255bW91cyIsInN1YiI6IjVkNTVlNGZhYTVjYTNlOWYzY2JjYmI4NmE5ZTRlNDFmNzdhZDRlZGY5ODg2OTA2Y2Y2NjVkMDdkNjdkZDQyZjgiLCJ0eXBlIjoxMH0.YE2121xWRS0te6BvtLOqLzoz-JiA6dhBzPqIzC-UuiQ"
+UI_URL = "https://www.chitai-gorod.ru"

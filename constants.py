c348804efb9c2fe233453d69cba6324a7bce4765
sevara3081 <sevara3081@mail.ru>
2025-01-14 21:24:48 +0300

    Delete constants.py

--- constants.py
+++
@@ -1,4 +0,0 @@
-BASE_API_URL = "https://web-gate.chitai-gorod.ru/api/v2"
-UI_URL = "https://www.chitai-gorod.ru"
-API_URL_SEARCH = "https://web-gate.chitai-gorod.ru/api/v2/search/search-phrase-suggests"
-BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzcwMjE2MjgsImlhdCI6MTczNjg1MzYyOCwiaXNzIjoiL2FwaS92MS9hdXRoL2Fub255bW91cyIsInN1YiI6IjgxMTIxNTMxNWQ0MTg0ZDhjM2Q2YmQzZjUyZmMzNGU2N2Q1OGQwODEwNWNhMjVkMDViNGNmOWEzMmI5OWEyNDgiLCJ0eXBlIjoxMH0.0KpHjsH7OKFShTkEi2BN9bjDJhnRIb3S9fEotXrHWcY"

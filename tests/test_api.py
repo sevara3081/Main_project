73990fcf7f76dc76752748cb891e41c2fdcf67a2
Sevara3081 <sevara3081@mail.ru>
2025-01-14 21:07:10 +0300

    test_api.py  new

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,7 @@
 import requests
 import allure
+from constants import BASE_API_URL, BEARER_TOKEN, API_URL_SEARCH
 
-# Константы
-API_URL_SEARCH = "https://web-gate.chitai-gorod.ru/api/v2/search/search-phrase-suggests"
-BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzY0ODcxOTgsImlhdCI6MTczNjMxOTE5OCwiaXNzIjoiL2FwaS92MS9hdXRoL2Fub255bW91cyIsInN1YiI6IjVkNTVlNGZhYTVjYTNlOWYzY2JjYmI4NmE5ZTRlNDFmNzdhZDRlZGY5ODg2OTA2Y2Y2NjVkMDdkNjdkZDQyZjgiLCJ0eXBlIjoxMH0.YE2121xWRS0te6BvtLOqLzoz-JiA6dhBzPqIzC-UuiQ"
 
 @allure.feature("Читай-город API")
 @allure.story("Поиск предложений по фразе (Позитивный тест)")
